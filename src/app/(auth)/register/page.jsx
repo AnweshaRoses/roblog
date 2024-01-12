0d82998056f88d235e9fa56e0ab5489c6b5b6aba
Anwesha Mishra <anwesharoses@gmail.com>
2024-01-12 20:07:26 +0530

    Error handling on register done

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,18 +1,13 @@
-import { register } from '@/lib/action'
+
 import React from 'react'
 import styles from './register.module.css'
+import RegisterForm from '@/components/registerform/RegisterForm'
 
 const RegisterPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-      <form className={styles.form} action={register}>
-        <input type="text" placeholder='Username' name="username"/>
-        <input type="email" placeholder='Email' name="email"/>
-        <input type="password" placeholder='Password' name="password"/>
-        <input type="password" placeholder='Password again' name="passwordRepeat"/>
-        <button>Register</button>
-      </form>
+        <RegisterForm/>
       </div>
     </div>
   )
--- src/lib/action.js
+++ src/lib/action.js
@@ -2,6 +2,7 @@
 import { connectToDb } from "./utils";
 import { Post, User } from "./models";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcryptjs"
 export const addPost=async(formData)=>{
 
     const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -46,25 +47,39 @@ export const deletePost = async (formData) => {
 
     await signOut()
   }
-  export const register = async(formData)=>{
+  export const register = async(previousState, formData)=>{
     const { username, email, password,img, passwordRepeat } = Object.fromEntries(formData);
     if(password!=passwordRepeat){
-      return "Passwords do not match!"
+      return {error:"Passwords do not match!"}
     }
     try{
       connectToDb()
       const user= await User.findOne({username})
       if(user){
-        return "User already exsists"
+        return {error:"User already exsists"}
       }
+      const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
       const newUser=new User({
         username,
         email,
-        password,
+        password:hashedPassword,
         img
     })
     await newUser.save();
     console.log("Saved to DB");
+    return {success:true}
+    }catch(err){
+      console.log(err);
+      return{error:"Something went wrong!"}
+    }
+  }
+
+  export const login = async(formData)=>{
+    const { username,  password } = Object.fromEntries(formData);
+
+    try{
+     await signIn("credentials",{username, password})
     }catch(err){
       console.log(err);
       return{error:"Something went wrong!"}
