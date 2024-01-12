b08bcc3cc380e84e441c397eb96391849e51fbbc
Anwesha Mishra <anwesharoses@gmail.com>
2024-01-12 05:53:04 +0530

    contact, home, about us done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Links from './links/Link'
 import styles from "./navbar.module.css"
+import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.logo}>
-            Logo
-        </div>
+        <Link href="/" className={styles.logo}>
+            Roblog
+        </Link>
         <div className="link">
             <Links/>
         </div>
